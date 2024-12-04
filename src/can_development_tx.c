631081a7079013f50b14ef05320a07391a7a06e8
Noah Mecham <nmecham27@gmail.com>
2024-12-04 13:31:51 -0700

    Everything is talking but not the low priority.

--- src/can_development_tx.c
+++ src/can_development_tx.c
@@ -11,6 +11,7 @@
 static struct can2040 cbus;
 static TaskHandle_t messageSenderTH;
 
+
 static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
 {
     // Put your code here....
@@ -47,5 +48,33 @@ void task1(void* args)
 
 int main(void)
 {
+    // initialization
+    stdio_init_all();
+
+    canbus_setup();
+
+    struct can2040_msg transmitMsg;
+
+    transmitMsg.id = 0x000F;
+    transmitMsg.dlc = 0x0008;
+    transmitMsg.data32[0] = 0x11223344;
+    transmitMsg.data32[1] = 0x55667788;
+
+    // gpio_init(1);
+    // gpio_set_dir(1,true);
+    // gpio_put(1, true);
+    
+    bool output = true;
+    while(1)
+    {
+        sleep_ms(1000);
+        printf("Trying to transmit\n");
+        // output = ~output;
+        // printf("output %u\n", output);
+        // gpio_put(5, output);
+        int returnValue = can2040_transmit(&cbus, &transmitMsg);
+        printf("Return value %i\n", returnValue);
+    }
+
     return 0;
 }
